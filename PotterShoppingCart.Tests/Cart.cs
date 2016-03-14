d522fb6a10da0ea7a008f0934705c913c24a242b
MilesLin <jkmejkme@yahoo.com.tw>
2016-03-15 00:08:46 +0800

    測試案例五:一次買了整套，一二三四五集各買了一本

--- PotterShoppingCart.Tests/Cart.cs
+++ PotterShoppingCart.Tests/Cart.cs
@@ -51,6 +51,9 @@ namespace PotterShoppingCart.Tests
                 case 4:
                     discount = 0.8;
                     break;
+                case 5:
+                    discount = 0.75;
+                    break;
             }
 
             return discount;
--- PotterShoppingCart.Tests/CartTest.cs
+++ PotterShoppingCart.Tests/CartTest.cs
@@ -150,5 +150,57 @@ namespace PotterShoppingCart.Tests
             //assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void CheckTest_Buy_5_Book_With_Different_Chapter()
+        {
+            //arrange
+            List<Book> books = new List<Book>()
+            {
+                new Book()
+                {
+                    SeriesNumber = 1,
+                    Name = "哈利波特第一集",
+                    Quantity = 1,
+                    Price = 100
+                },new Book()
+                {
+                    SeriesNumber = 2,
+                    Name = "哈利波特第二集",
+                    Quantity = 1,
+                    Price = 100
+                },
+                new Book()
+                {
+                    SeriesNumber = 3,
+                    Name = "哈利波特第三集",
+                    Quantity = 1,
+                    Price = 100
+                },new Book()
+                {
+                    SeriesNumber = 4,
+                    Name = "哈利波特第四集",
+                    Quantity = 1,
+                    Price = 100
+                },new Book()
+                {
+                    SeriesNumber = 5,
+                    Name = "哈利波特第五集",
+                    Quantity = 1,
+                    Price = 100
+                },
+                
+            };
+
+            int expected = 375;
+
+            var target = new Cart();
+
+            //act
+            int actual = target.Check(books);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
